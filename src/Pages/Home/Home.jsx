d42db0bde2ee10eabc34b27c11b7bf3fa85cf017
Tushar Malankiya <85736626+tusharmalankiya@users.noreply.github.com>
2024-08-18 18:03:13 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,18 +7,52 @@ import HomeSidebar from "../../components/HomeSidebar/HomeSidebar";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const IMG_URL =
-  "https://media.istockphoto.com/id/1349239413/photo/shot-of-coffee-beans-and-a-cup-of-black-coffee-on-a-wooden-table.jpg?b=1&s=170667a&w=0&k=20&c=0bVq4jWM5d6r4Klp0si4um7QjZIQkMjYLtuDU7oWUps=";
+// const IMG_URL =
+//   "https://media.istockphoto.com/id/1349239413/photo/shot-of-coffee-beans-and-a-cup-of-black-coffee-on-a-wooden-table.jpg?b=1&s=170667a&w=0&k=20&c=0bVq4jWM5d6r4Klp0si4um7QjZIQkMjYLtuDU7oWUps=";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
+  const [randnum, setRandNum] = useState(0);
 
-  // useEffect(()=>{
-  //   axios.get("http://localhost:4000/api/v1").then((res)=>{
-  //     setBlogs(res.data);
-  //   })
+  //development test just to get and post random num from server to the server
+  useEffect(()=>{
+    const fetchRandNum = async () =>{
+      try{
+        const resp = await axios.get("http://localhost:4000/api/v1/dev/randnum");
+        console.log(resp.data);
+        setRandNum(resp.data.num);
+        await axios.post("http://localhost:4000/api/v1/dev/randnum", {num: resp.data.num}, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      }catch(err){
+        console.error("error while fetching random num", err);
+      }
+    }
+    fetchRandNum();
 
-  // }, [])
+    // const Interval = setInterval(()=>{
+    //   if(randnum !== 0 || randnum <= 100){
+    //     fetchRandNum();
+    //   }
+    // }, 100);
+
+    // return ()=>{
+    //   clearInterval(Interval);
+    // }
+
+  }, []);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  useEffect(()=>{
+    axios.get("http://localhost:4000/api/v1/dev").then((res)=>{
+      setBlogs(res.data);
+      console.log(res.data);
+    })
+
+  }, [])
 
   return (
     <>
@@ -37,7 +71,8 @@ const Home = () => {
         </div>
 
         <hr />
-
+        <h1>{randnum}</h1>
+        
         <div className="home-main">
           <div className="home-main-left">
             {/* <BlogPost blog_img={IMG_URL} profile_img={IMG_URL} />
@@ -50,24 +85,23 @@ const Home = () => {
             <BlogPost blog_img={IMG_URL} profile_img={IMG_URL} />
             <BlogPost blog_img={IMG_URL} profile_img={IMG_URL} /> */}
             {}
-            <BlogPost
+            {/* <BlogPost
              author="Author"
              title="What I Learned as a Product Designer at Apple" 
              desc="  In 2021 I landed my dream job. Working at Apple, the holy grail of
              minimalistic design, innovation and creativity. A place where"
             blog_img={IMG_URL}
-             profile_img="no" />
+             profile_img={IMG_URL} /> */}
 
-             {/* {blogs.map((blog)=>{
+             {blogs.map((blog)=>{
               return <BlogPost
               key={blog._id}
               author={blog.firstname}
               title={blog.title} 
-              desc="  In 2021 I landed my dream job. Working at Apple, the holy grail of
-              minimalistic design, innovation and creativity. A place where"
-             blog_img={'http://localhost:4000' +blog.image}
-              profile_img={'http://localhost:4000' +blog.image} />
-             })} */}
+              desc={blog.desc}
+             blog_img={blog.image}
+              profile_img={blog.image} />
+             })}
             
 
             {/* //--------load more button --------------------------// */}
